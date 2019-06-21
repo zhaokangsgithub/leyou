2dac43295457e83e1e5593b7cfe184e62c5c0db9
zhaokang <zhaokang@hzzl.net>
2019-06-21 17:52:20 +0800

    版本提交

---
+++ ly-registry/src/main/java/com/leyou/LyRegistry.java
@@ -0,0 +1,22 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * ClassName:  <br/>
+ * Function:  ADD FUNCTION. <br/>
+ * Reason:  ADD REASON(可选). <br/>
+ * date:   <br/>
+ *
+ * @author
+ * @since JDK 1.6
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LyRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(LyRegistry.class, args);
+    }
+}
