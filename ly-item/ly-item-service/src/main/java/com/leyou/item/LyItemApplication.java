133f9871587f7ec0b72626ca194fddfc4901c6d4
zhaokang <zhaokang@hzzl.net>
2019-07-18 19:35:54 +0800

    leyou基础搭建

--- ly-item/ly-item-service/src/main/java/com/leyou/item/LyItemApplication.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/LyItemApplication.java
@@ -2,6 +2,7 @@ package com.leyou.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
