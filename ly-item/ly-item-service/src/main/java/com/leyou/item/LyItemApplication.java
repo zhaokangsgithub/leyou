bc29374226b95086cb10e2a9588d7130881e960d
ktt <15656072458@163.com>
2019-07-29 22:11:56 +0800

    api网关实现文件上传后台 zhaokang

--- ly-item/ly-item-service/src/main/java/com/leyou/item/LyItemApplication.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/LyItemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * ClassName:  <br/>
@@ -16,6 +17,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.leyou.item.mapper")
 public class LyItemApplication
 {
     public static void main(String[] args)
